ba26e5d1b47b215faef9d13cbc753366cd3d0865
Sanjanarajbk <gowdasanjana60.sg@gmail.com>
2023-12-23 09:33:50 +0530

    [Sanjana] Header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () =>{
+    return(
+        <>
+        <header className='header'>
+            <h2>To Do App</h2>
+        </header>
+        </>
+    )
+}
+
+export default Header;

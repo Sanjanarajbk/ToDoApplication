ec8249c9052653f31e5b1dd18f9bc5d08bfdc771
Sanjanarajbk <gowdasanjana60.sg@gmail.com>
2023-12-23 09:46:24 +0530

    [Sanjana] Rendering the list with unique key

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 const ToDoListItem = () =>{
+    const listItems =["Read SpringBoot", "Complete assignments", "Prepare breakfast", 
+    "Sleep for 2 hours", "Take a shower"]
+
     return(
         <>
-<p>
-Nothing to do buddy. Sleep!!
-</p>
+        {
+            listItems.map((element,index) => {
+             return   <p key={index}>
+                    {element}
+                </p>
+            })
+        }
 </>
     )
 }
